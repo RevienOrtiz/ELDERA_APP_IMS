f85bccf11e7a5646bdafdfebeab51497044ac022
Sheen Macdames Soriano <tripwapak@gmail.com>
2025-12-04 21:58:59 +0800

    last logo fix

--- ELDERA_IMSmodified/ELDERA_IMS/resources/views/index.blade.php
+++ ELDERA_IMSmodified/ELDERA_IMS/resources/views/index.blade.php
@@ -242,7 +242,7 @@
 </head>
 
 <body>
-    <img src="{{asset('images/LCSCF_LOGO.png')}}" alt="LCSCF Logo" class="logo">
+    <img src="{{asset('images/LCSCF_Logo.png')}}" alt="LCSCF Logo" class="logo">
     
     <main>
         <div class="container">
